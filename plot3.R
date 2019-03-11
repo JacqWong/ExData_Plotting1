e5a9a3aa904944ff4c5fac90ed96bb15c6626656
Your NameJacqueline <wong@essb.eur.nl>
2019-03-11 13:13:23 +0100

    Plot 3

--- plot3.R
+++ plot3.R
@@ -24,6 +24,6 @@ legend("topright"
        , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
        ,lty=c(1,1), lwd=c(1,1))
 
-#save as png 
+#save as png  
 dev.copy(png, 'plot3.png')
 dev.off()
